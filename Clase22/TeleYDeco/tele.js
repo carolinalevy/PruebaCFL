8d75dc168ca6aa5c03444da13176403f8d7537d7
carolinalevy <carolina.z.levy@gmail.com>
2020-07-27 15:23:18 -0300

    ejercicios Clase22

---
+++ Clase22/TeleYDeco/tele.js
@@ -0,0 +1,63 @@
+var Televisor = /** @class */ (function () {
+    function Televisor(decodificador) {
+        this.decodificador = decodificador;
+    }
+    Televisor.prototype.prenderApagar = function () {
+        if (this.estaPrendido) {
+            this.estaPrendido = false;
+        }
+        else {
+            this.estaPrendido = true;
+        }
+    };
+    Televisor.prototype.subirVolumen = function () {
+        if (this.estaPrendido) {
+            this.decodificador.subirVolumen();
+        }
+    };
+    Televisor.prototype.bajarVolumen = function () {
+        if (this.estaPrendido) {
+            this.decodificador.bajarVolumen();
+        }
+    };
+    Televisor.prototype.subirCanal = function () {
+        if (this.estaPrendido) {
+            this.decodificador.subirCanal();
+        }
+    };
+    Televisor.prototype.bajarCanal = function () {
+        if (this.estaPrendido) {
+            this.decodificador.bajarCanal();
+        }
+    };
+    return Televisor;
+}());
+var Decodificador = /** @class */ (function () {
+    function Decodificador(volumenActual, canalActual) {
+        this.volumenActual = volumenActual;
+        this.canalActual = canalActual;
+    }
+    Decodificador.prototype.subirVolumen = function () {
+        this.volumenActual = this.volumenActual + 1;
+    };
+    Decodificador.prototype.bajarVolumen = function () {
+        this.volumenActual = this.volumenActual - 1;
+    };
+    Decodificador.prototype.subirCanal = function () {
+        this.canalActual = this.canalActual + 1;
+    };
+    Decodificador.prototype.bajarCanal = function () {
+        this.canalActual = this.canalActual - 1;
+    };
+    return Decodificador;
+}());
+var decodificador = new Decodificador(4, 22);
+var primerTelevisor = new Televisor(decodificador);
+console.log(decodificador);
+console.log(primerTelevisor);
+primerTelevisor.prenderApagar();
+primerTelevisor.bajarCanal();
+primerTelevisor.bajarVolumen();
+primerTelevisor.bajarVolumen();
+console.log(decodificador);
+console.log(primerTelevisor);
